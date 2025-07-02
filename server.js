9bd42c9d783960bb1dc030b0bac34cb0c6e5435f
Seif Eldin <seifeldinn4@gmail.com>
2025-07-02 06:06:00 +0300

    Add / in server.js 2

--- server.js
+++ server.js
@@ -13,15 +13,14 @@ app.use('/api', routes);
 
 // console.log("JWT_SECRET_KEY:", process.env.JWT_SECRET_KEY);-
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: '✅ Welcome To Expense Tracker Backend Server!' });
+});
 
 // handler for invalid API routing
 app.all('*', (req, resp, next) => {
   return resp.status(500).json({ ststus: httpStatusText.ERROR, message: 'This resource is not available', code: 500 })
 })
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: '✅ Welcome To Expense Tracker Backend Server!' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
